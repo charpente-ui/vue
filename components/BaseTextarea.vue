7d3afeda6570a375a7aa2328d8ba3955db01c722
Boris Damevin <bdamevin@frontfactory.dev>
2026-02-26 14:24:02 +0100

    feat(textarea): add component

---
+++ components/BaseTextarea.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useId, useAttrs } from 'vue';
+
+defineOptions({
+    inheritAttrs: false
+});
+
+const model = defineModel<string>();
+const attrs = useAttrs();
+const textareaId = (attrs.id as string) || useId();
+</script>
+
+<template>
+    <textarea :id="textareaId" v-model="model" v-bind="$attrs"/>
+</template>
+
+<style scoped>
+
+</style>
