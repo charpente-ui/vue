8f56d3bb6586d5fc2780eb8134a6b2a29ad63b6d
Boris Damevin <bdamevin@frontfactory.dev>
2026-02-26 12:51:05 +0100

    feat(label): add component

---
+++ components/BaseLabel.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useId, useAttrs } from 'vue';
+
+defineOptions({
+    inheritAttrs: false
+});
+
+const attrs = useAttrs();
+const labelId = (attrs.id as string) || useId();
+</script>
+
+<template>
+    <label :id="labelId" v-bind="$attrs">
+        <slot/>
+    </label>
+</template>
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { default as CInput } from '../components/BaseInput.vue';
+export { default as CLabel } from '../components/BaseLabel.vue';
