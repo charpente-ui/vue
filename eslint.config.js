cf26ee5959cc49b73c45f0d831774109cad194e8
Boris Damevin <bdamevin@frontfactory.dev>
2026-02-27 13:23:04 +0100

    chore(deps-dev): add `@front-factory/eslint-config` 1.3.1

---
+++ eslint.config.js
@@ -0,0 +1,79 @@
+// ============================================================================================= //
+//                                            ESLINT                                             //
+// ============================================================================================= //
+
+import { defineConfig } from 'eslint/config';
+import frontFactoryConfig from '@front-factory/eslint-config';
+import ts from 'typescript-eslint';
+import vuePlugin from 'eslint-plugin-vue';
+
+export default defineConfig([
+    {
+        ignores: [
+            'dist/**',
+            'node_modules/**'
+        ]
+    },
+    ...ts.configs.recommended,
+    ...vuePlugin.configs['flat/recommended'],
+    ...frontFactoryConfig,
+    {
+        files: [
+            '**/*.vue'
+        ],
+        languageOptions: {
+            parser: vuePlugin.parser,
+            parserOptions: {
+                parser: ts.parser,
+                sourceType: 'module',
+                extraFileExtensions: ['.vue']
+            }
+        }
+    },
+    {
+        plugins: {
+            'vue': vuePlugin
+        },
+        rules: {
+            'vue/first-attribute-linebreak': [
+                'error',
+                {
+                    'singleline': 'ignore',
+                    'multiline': 'beside'
+                }
+            ],
+            'vue/html-closing-bracket-newline': [
+                'error',
+                {
+                    'singleline': 'never',
+                    'multiline': 'never'
+                }
+            ],
+            'vue/html-closing-bracket-spacing': [
+                'error',
+                {
+                    'selfClosingTag': 'never'
+                }
+            ],
+            'vue/html-indent': [
+                'error',
+                4
+            ],
+            'vue/html-self-closing': [
+                'error',
+                {
+                    'html': {
+                        'void': 'always',
+                        'normal': 'always',
+                        'component': 'always'
+                    },
+                    'svg': 'always',
+                    'math': 'always'
+                }
+            ],
+            'vue/max-attributes-per-line': 'off',
+            'vue/multi-word-component-names': 'off',
+            'vue/singleline-html-element-content-newline': 'off'
+        }
+    }
+]);
