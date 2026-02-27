95b5a2f51c3949df5b95c15b025fa6a6dbb6df4c
Boris Damevin <bdamevin@frontfactory.dev>
2026-02-27 10:05:05 +0100

    feat(select): add headless component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed, useId, useAttrs } from 'vue';
+
+defineOptions({
+    inheritAttrs: false
+});
+
+const model = defineModel<any>();
+const attrs = useAttrs();
+
+const selectId = computed(() => {
+    return (attrs.id as string) || useId();
+});
+</script>
+
+<template>
+    <select v-bind="$attrs" v-model="model" :id="selectId">
+        <slot/>
+    </select>
+</template>
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as CCheckbox } from './components/BaseCheckbox.vue';
 export { default as CInput } from './components/BaseInput.vue';
 export { default as CLabel } from './components/BaseLabel.vue';
 export { default as CRadio } from './components/BaseRadio.vue';
+export { default as CSelect } from './components/BaseSelect.vue';
 export { default as CTextarea } from './components/BaseTextarea.vue';
